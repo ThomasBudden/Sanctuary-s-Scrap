db71ad78071a527586680d616618548f383aee26
ThomasBudden <buddenthomas04@gmail.com>
2025-10-22 11:50:08 +0100

    More level changes

--- Sanctuary's Scrap/Assets/Scripts/GameManagerScript.cs
+++ Sanctuary's Scrap/Assets/Scripts/GameManagerScript.cs
@@ -8,6 +8,7 @@ public class GameManagerScript : MonoBehaviour
     public GameObject enemy;
     public EnemyScriptable[] enemyStats;
     public GameObject[] roomArray;
+    public GameObject currentRoom;
     public List<GameObject> enemySpawns;
     private float startTime;
     public List<GameObject> usedSpawns;
@@ -19,23 +20,29 @@ public class GameManagerScript : MonoBehaviour
 
     void Start()
     {
-        wave = 0;
-        int roomRand = Random.Range(0, roomArray.Length);
-        Instantiate(roomArray[roomRand], new Vector3(0,0,0), Quaternion.identity);
-        player.transform.position = roomArray[roomRand].transform.GetChild(0).transform.position;
-        for (int i = 0; i < roomArray[roomRand].transform.GetChild(1).childCount; i++)
-        {
-            enemySpawns.Add(roomArray[roomRand].transform.GetChild(1).GetChild(i).gameObject);
-        }
+        roomSpawn();
         startTime = Time.time;
     }
 
-    // Update is called once per frame
+    public void roomSpawn()
+    {
+        if (enemySpawns.Count != 0)
+        {
+            enemySpawns.Clear();
+        }
+        wave = 0;
+        int roomRand = Random.Range(0, roomArray.Length);
+        currentRoom = Instantiate(roomArray[roomRand], new Vector3(0, 0, 0), Quaternion.identity);
+        player.transform.position = currentRoom.transform.GetChild(0).transform.position;
+        for (int i = 0; i < currentRoom.transform.GetChild(1).childCount; i++)
+        {
+            enemySpawns.Add(roomArray[roomRand].transform.GetChild(1).GetChild(i).gameObject);
+        }
+    }
     void Update()
     {
-        if (wave == 0 && enemysActive <= 0)
+        if (wave < 2 && enemysActive <= 0)
         {
-            //wave = wave + 1;
             usedSpawns = new List<GameObject>();
             spawnTest = Random.Range((enemySpawns.Count / 2), enemySpawns.Count);
             for (int i = 0; i < spawnTest; i++)
@@ -76,6 +83,12 @@ public class GameManagerScript : MonoBehaviour
                     break;
                 }
             }
+            wave = wave + 1;
+        }
+        else if (wave >= 2)
+        {
+            Destroy(currentRoom.gameObject);
+            roomSpawn();
         }
     }
 }
